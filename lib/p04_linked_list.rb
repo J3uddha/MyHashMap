b7f09371382fd0b806d335629e13f405d67a4e42
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2015-10-09 18:20:23 -0400

    finish linked_list class

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,4 +1,8 @@
+include Enumerable
+
 class Link
+
+
   attr_accessor :key, :val, :next
 
   def initialize(key = nil, val = nil, nxt = nil)
@@ -26,24 +30,50 @@ class LinkedList
   end
 
   def last
+    @last = self[-1]
   end
 
   def empty?
   end
 
   def get(key)
+    link = @head
+
+    loop do
+      return nil if link == @last
+      return link.val if link.key == key
+      link = link.next
+    end
   end
 
   def include?(key)
+    link = @head
+
+    loop do
+      return false if link == @last
+      return true if link.key == key
+      link = link.next
+    end
   end
 
   def insert(key, val)
+    @head.next = Link.new(key, val, @head.next)
   end
 
   def remove(key)
+    link = @head
+
+    until link == @last
+      link_next = link.next
+      if !link.next.nil? && link_next.key == key
+        link.next = link_next.next ? link_next.next : nil
+      end
+      link = link_next
+    end
   end
 
   def each
+    self.each
   end
 
   # uncomment when you have `each` working and `Enumerable` included
