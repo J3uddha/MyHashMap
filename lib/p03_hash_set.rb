205f1272b72f92fbe69602092efd6a734ebc47b3
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2015-10-09 16:17:49 -0400

    complete hashmap class

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -1,4 +1,5 @@
 require_relative 'p02_hashing'
+require 'json'
 
 class HashSet
   attr_reader :count
@@ -8,13 +9,27 @@ class HashSet
     @count = 0
   end
 
+  def mod(key)
+    key.to_json.hash % num_buckets
+  end
+
   def insert(key)
+    if @count >= num_buckets
+      resize!
+    end
+    @store[mod(key)] << key.hash
+    @count += 1
   end
 
   def include?(key)
+    @store[mod(key)].any? {|int| int == key.hash }
   end
 
   def remove(key)
+    bucket = @store[mod(key)]
+    i = bucket.index(key.hash)
+    bucket[i] = nil
+    @count -= 1
   end
 
   private
@@ -28,5 +43,15 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    new_size = old_store.size * 2
+    @store = Array.new(new_size) { [] }
+    old_store.each do |bucket|
+      bucket.each do |el|
+        @store[mod(el)] << el
+      end
+    end
+
+    nil
   end
 end
