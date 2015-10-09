231731da080e30cb74604bdfb81dd0868ed2a26e
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2015-10-09 15:53:29 -0400

    add hash methods for data types

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,15 +4,31 @@ end
 
 class Array
   def hash
+    hash = 0
+    self.each_with_index do |num, i|
+      hash = hash ^ (num.hash + i)
+    end
+
+    hash
   end
 end
 
 class String
   def hash
+    letters = []
+    self.chars.each {|l| letters << l.ord}
+
+    letters.hash
   end
 end
 
 class Hash
   def hash
+    hash = 0
+    self.values.each do |v|
+      hash += v.hash
+    end
+
+    hash
   end
 end
